facc9bf5e625656b7d7cd270760c8a6df7ae0ca9
Alex Xu <a.i.xu@me.com>
2019-02-09 16:23:35 -0500

    Tic-Tac-Toe

--- gym_rex/envs/rex_env.py
+++ gym_rex/envs/rex_env.py
@@ -3,17 +3,86 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class RexEnv(gym.Env):
-    metadata = {'render.modes' : ['human']}
+	metadata = {'render.modes' : ['human']}
 
-    def __init__(self):
-        ...
+	def __init__(self):
+		self.state = []
+		# create the game board
+		for i in range(3):
+			self.state += [[]]
+			for j in range(3):
+				self.state[i] += ["-"]
+		self.counter = 0
+		self.done = 0
+		self.add = [0, 0]
+		self.reward = 0
 
-    def step(self, action):
-        ...
+	def check(self):
+		if(self.counter<5):
+			return 0
+		for i in range(3):
+			if(self.state[i][0] != "-" and self.state[i][1] == self.state[i][0] and self.state[i][1] == self.state[i][2]):
+				if(self.state[i][0] == "o"):
+					return 1
+				else:
+					return 2
+			if(self.state[0][i] != "-" and self.state[1][i] == self.state[0][i] and self.state[1][i] == self.state[2][i]):
+				if(self.state[0][i] == "o"):
+					return 1
+				else:
+					return 2
+		if(self.state[0][0] != "-" and self.state[1][1] == self.state[0][0] and self.state[1][1] == self.state[2][2]):
+			if(self.state[0][0] == "o"):
+				return 1
+			else:
+				return 2
+		if(self.state[0][2] != "-" and self.state[0][2] == self.state[1][1] and self.state[1][1] == self.state[2][0]):
+			if(self.state[1][1] == "o"):
+				return 1
+			else:
+				return 2
 
-    def reset(self):
-        ...
+	def step(self, action):
+		if self.done == 1:
+			print("Game Over")
+			return [self.state, self.reward, self.done, self.add]
+		elif self.state[action//3][action%3] != "-":
+			print("Invalid Step")
+			return [self.state, self.reward, self.done, self.add]
+		else:
+			if self.counter % 2 == 0:
+				self.state[action//3][action%3] == "o"
+			else:
+				self.state[action//3][action%3] = "x"
+			self.counter += 1
+			if self.counter == 9:
+				self.done = 1
+			self.render()
 
-    def render(self, mode='human', close=False):
-        ...
+			win = self.check()
+			if win :
+				self.done = 1
+				print("Player ", win, " wins.", sep="", end="\n")
+				self.add[win-1] = 1
+				if win == 1:
+					self.reward = 100
+				else:
+					self.reset = -100
+			return [self.state, self.reward, self.done, self.add]
+
+	def reset(self):
+		for i in range(3):
+			for j in range(3):
+				self.state[i][j] = "-"
+		self.counter = 0
+		self.done = 0
+		self.add = [0, 0]
+		self.reward = 0
+		return self.state
+
+	def render(self, mode='human', close=False):
+		for i in range(3):
+			for j in range(3):
+				print(self.state[i][j], end=" ")
+			print("")
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_rex',
-      version='0.1',
-      install_requires=['gym']
+	version='0.0.1',
+	install_requires=['gym']
 )
