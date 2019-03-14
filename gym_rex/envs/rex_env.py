068eeba1edaaef47bba48510c186de64152d4470
Alex Xu <a.i.xu@me.com>
2019-03-14 02:30:38 -0400

    Cleaned unused code and comments

--- gym_rex/envs/rex_env.py
+++ gym_rex/envs/rex_env.py
@@ -33,41 +33,6 @@ class RexEnv(gym.Env):
 		self.observation_space = spaces.Box(0, 255, (210, 160, 3), dtype=np.uint8)
 		self.gameBoard = initGameBoard()
 		self.state = stateGrid2Img(self.gameBoard, np.array(self.game.state.data.food.data), self.game.state.data.agentStates[0])
-		# # create the game board -- Tic-Tac-Toe
-		# self.state = []
-		# for i in range(3):
-		# 	self.state += [[]]
-		# 	for j in range(3):
-		# 		self.state[i] += ["-"]
-		# self.counter = 0
-		# self.done = 0
-		# self.add = [0, 0]
-		# self.reward = 0
-
-	# def check(self):
-	# 	if(self.counter<5):
-	# 		return 0
-	# 	for i in range(3):
-	# 		if(self.state[i][0] != "-" and self.state[i][1] == self.state[i][0] and self.state[i][1] == self.state[i][2]):
-	# 			if(self.state[i][0] == "o"):
-	# 				return 1
-	# 			else:
-	# 				return 2
-	# 		if(self.state[0][i] != "-" and self.state[1][i] == self.state[0][i] and self.state[1][i] == self.state[2][i]):
-	# 			if(self.state[0][i] == "o"):
-	# 				return 1
-	# 			else:
-	# 				return 2
-	# 	if(self.state[0][0] != "-" and self.state[1][1] == self.state[0][0] and self.state[1][1] == self.state[2][2]):
-	# 		if(self.state[0][0] == "o"):
-	# 			return 1
-	# 		else:
-	# 			return 2
-	# 	if(self.state[0][2] != "-" and self.state[0][2] == self.state[1][1] and self.state[1][1] == self.state[2][0]):
-	# 		if(self.state[1][1] == "o"):
-	# 			return 1
-	# 		else:
-	# 			return 2
 
 	def step(self, action):
 		action = list(Actions._directions.keys())[action]	# action format from number to string
@@ -80,33 +45,11 @@ class RexEnv(gym.Env):
 		self.state = stateGrid2Img(self.gameBoard, np.array(self.game.state.data.food.data), self.game.state.data.agentStates[0])
 		self.reward = self.game.state.getScore()
 		self.done = self.game.gameOver
+		if self.game.numMoves > 300:
+			self.done = True
+			self.info['info'] = 'Failed to complete. Too many moves.'
+			self.info['numMoves'] = self.game.numMoves
 		return [self.state, self.reward, self.done, self.info]
-		# if self.done == 1:
-		# 	print("Game Over")
-		# 	return [self.state, self.reward, self.done, self.info]
-		# elif self.state[action//3][action%3] != "-":
-		# 	print("Invalid Step")
-		# 	return [self.state, self.reward, self.done, self.info]
-		# else:
-		# 	if self.counter % 2 == 0:
-		# 		self.state[action//3][action%3] = "o"
-		# 	else:
-		# 		self.state[action//3][action%3] = "x"
-		# 	self.counter += 1
-		# 	if self.counter == 9:
-		# 		self.done = 1
-		# 	self.render()
-
-		# 	win = self.check()
-		# 	if win :
-		# 		self.done = 1
-		# 		print("Player ", win, " wins.", sep="", end="\n")
-		# 		self.info[win-1] = 1
-		# 		if win == 1:
-		# 			self.reward = 100
-		# 		else:
-		# 			self.reset = -100
-		#	return [self.state, self.reward, self.done, self.info]
 
 	def reset(self):
 		args = readCommand( sys.argv[1:] ) # Get game components based on input
@@ -123,21 +66,13 @@ class RexEnv(gym.Env):
 		self.gameBoard = initGameBoard()
 		self.state = stateGrid2Img(self.gameBoard, np.array(self.game.state.data.food.data), self.game.state.data.agentStates[0])
 		return self.state
-		# for i in range(3):
-		# 	for j in range(3):
-		# 		self.state[i][j] = "-"
-		# self.counter = 0
 
 	def render(self, mode='human', close=False):
 		# self.game.state.data.agentStates[0].getPosition()	# current pacman position
-		print(self.game.state)
+		# print(self.game.state)
 		cv2.imshow("Recman - Score: " + str(self.reward), self.state)
 		cv2.waitKey(10)
 		cv2.destroyAllWindows()
-		# for i in range(3):
-		# 	for j in range(3):
-		# 		print(self.state[i][j], end=" ")
-		# 	print("")
 
 	def get_action_meanings(self):
 		return list(Actions._directions.keys())
@@ -153,21 +88,6 @@ def stateGrid2Img(gameBoard, foodData, pacman_pos):
 	pt3 = (8, 6)
 	triangle = np.array( [pt1, pt2, pt3] )
 	cv2.drawContours(triangle_man, [triangle], 0, (0,255,0), -1)
-	# if pacman_pos.getDirection() == 'South':
-	# 	triangle_man = np.rot90(triangle_man)
-	# 	triangle_man = np.rot90(triangle_man)
-	# elif pacman_pos.getDirection() == 'East' or pacman_pos.getDirection() == 'West':
-	# 	pt1 = (0, 0)
-	# 	pt2 = (5, 7)
-	# 	pt3 = (10, 0)
-	# 	triangle_man = np.zeros((11, 7, 3), np.uint8) * 255
-	# 	triangle = np.array( [pt1, pt2, pt3] )
-	# 	cv2.drawContours(triangle_man, [triangle], 0, (0,255,0), -1)
-	# 	if pacman_pos.getDirection() == 'West':
-	# 		triangle_man = np.rot90(triangle_man)
-	# 		triangle_man = np.rot90(triangle_man)
-	# else:
-	# 	pass
 	pos = pacman_pos.getPosition()
 	pos_row = 14 - pos[1]
 	pos_col = pos[0]
@@ -215,24 +135,3 @@ def initGameBoard():
 			if j > 76: j = j - 2
 			foodPattern[2+i:14+i, 3+j:10+j] = foodImg
 	return img+foodPattern
-
-
-# import cv2
-# import numpy as np
-
-# image = np.ones((300, 300, 3), np.uint8) * 255
-
-# pt1 = (150, 100)
-# pt2 = (100, 200)
-# pt3 = (200, 200)
-
-# cv2.circle(image, pt1, 2, (0,0,255), -1)
-# cv2.circle(image, pt2, 2, (0,0,255), -1)
-# cv2.circle(image, pt3, 2, (0,0,255), -1)
-
-# triangle_cnt = np.array( [pt1, pt2, pt3] )
-
-# cv2.drawContours(image, [triangle_cnt], 0, (0,255,0), -1)
-
-# cv2.imshow("image", image)
-# cv2.waitKey()
