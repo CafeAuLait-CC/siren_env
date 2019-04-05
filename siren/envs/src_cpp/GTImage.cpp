74442021ff82ee0f2222b11ecc1499ec151f5ecf
Alex Xu <a.i.xu@me.com>
2019-04-05 15:38:41 -0400

    Added travel path to chessboard edges

--- siren/envs/src_cpp/GTImage.cpp
+++ siren/envs/src_cpp/GTImage.cpp
@@ -27,16 +27,28 @@ GTImage::GTImage(std::string& fileName, cv::Size cellSize, cv::Size pixelSize) {
             }
             cv::Vec3b value = pixelImage.at<cv::Vec3b>(i, j);
             if (value == cv::Vec3b(0, 0, 0)) {
-                cellImage.at<uchar>(i/cellSize.height, j/cellSize.width) = 0;   // nothing
+                cellImage.at<uchar>(i/cellSize.height, j/cellSize.width) = 0;   // label 0 for nothing (black pixels, including non-centerlines and unknown labels)
             } else if (value == cv::Vec3b(0, 255, 0)) {
-                cellImage.at<uchar>(i/cellSize.height, j/cellSize.width) = 1;   // road
+                cellImage.at<uchar>(i/cellSize.height, j/cellSize.width) = 1;   // label 1 for road
             } else if (value == cv::Vec3b(255, 0, 0)) {
-                cellImage.at<uchar>(i/cellSize.height, j/cellSize.width) = 0;   // building
+                cellImage.at<uchar>(i/cellSize.height, j/cellSize.width) = 0;   // label 0 (or 2) for building
             } else {
                  std::cerr << "Ground truth image pixel value wrong: " << i << ", " << j << " " << value << std::endl;;
                  exit(-1);
             }
             
+        }
+    }
+    
+    // Add a travel path to the edges
+    // When agent reaches an edge, use the travel path to find next entry point.
+    for (int i = 0; i < cellImage.rows; i++) {
+        if (i > 0 && i < cellImage.rows - 1) {  // not the first or last row, only label the left and right edges.
+            cellImage.at<uchar>(i, 0) = 4;      // label 4 for travel path
+            cellImage.at<uchar>(i, cellImage.cols - 1) = 4;
+        } else {        // the first and last row, label all pixel as 4
+            cv::Mat allOnesRow = cv::Mat::ones(1, cellImage.cols, 0) * 4;
+            allOnesRow.copyTo(cellImage(cv::Range(cellImage.rows - 1, cellImage.rows), cv::Range(0, cellImage.cols)));
         }
     }
 }
